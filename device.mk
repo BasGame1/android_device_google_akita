31248e85755ec33a727fa0ed96eed36e1f0dfe6d
BasGame1 <abadbasantaalex@gmail.com>
2025-06-13 15:17:59 +0200

    Initial commit: Basic device tree for Pixel 8a (akita)

---
+++ device.mk
@@ -0,0 +1,8 @@
+PRODUCT_COPY_FILES += \
+    $(DEVICE_PATH)/recovery.fstab:$(TARGET_COPY_OUT_RECOVERY)/root/etc/recovery.fstab
+
+PRODUCT_DEVICE := akita
+PRODUCT_NAME := twrp_akita
+PRODUCT_BRAND := google
+PRODUCT_MODEL := Pixel 8a
+PRODUCT_MANUFACTURER := Google
---
+++ twrp_akita.mk
@@ -0,0 +1,3 @@
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
+$(call inherit-product, device/google/akita/device.mk)
+$(call inherit-product, vendor/twrp/config/common.mk)
